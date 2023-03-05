e290af8e506d2b79a2f7a2dc2a8010709a6860e8
NagarjunaBasam <basam.nagarjuna@gmail.com>
2023-03-05 22:08:47 +0400

    feat: Added news details page

---
+++ HRA.News.Web/HRA.News.Web/Pages/News/Details.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRA.News.Infrastructure.Interfaces;
+using HRA.News.Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRA.News.Web.Pages.News
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IArticlesRepository _articlesRepository;
+        public Article Article { get; set; }
+        public DetailsModel(IArticlesRepository articleRepository)
+        {
+            this._articlesRepository = articleRepository;
+        }
+
+        public void OnGet(int id, string language)
+        {
+            Article = _articlesRepository.GetArticle(id);
+            TempData["HideSearchFilter"] = true;
+            TempData["language"] = language;
+        }
+    }
+}
