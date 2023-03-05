768baded9f1dbfde95d77ea5df0c52fefef64e81
NagarjunaBasam <basam.nagarjuna@gmail.com>
2023-03-05 22:47:15 +0400

    feat: Added search, language and refresh buttons

---
+++ HRA.News.Web/HRA.News.Web/ViewComponents/SearchLanguageFilterViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using HRA.News.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+
+namespace HRA.News.Web.ViewComponents
+{
+    public class SearchLanguageFilterViewComponent : ViewComponent
+    {
+        private readonly IArticlesBusinessLayer _articlesBusinessLayer;
+        public SearchLanguageFilterViewComponent(IArticlesBusinessLayer articlesBusinessLayer)
+        {
+            this._articlesBusinessLayer = articlesBusinessLayer;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var result = _articlesBusinessLayer.GetLanguagesDrps().Select(a =>
+                                  new SelectListItem
+                                  {
+                                      Value = a.Value,
+                                      Text = a.Text,
+                                      Selected = TempData["language"] != null && TempData["language"].ToString() == a.Value,
+                                  }).ToList();
+            return View(result);
+        }
+
+    }
+}
